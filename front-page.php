08268f1f1a220041bd20d4be883210a3b5fa3c50
Mike iLL Kilmer <mike@mzoo.org>
2016-05-05 12:50:46 -0500

    Begin styling the header for media queries.

--- front-page.php
+++ front-page.php
@@ -12,13 +12,21 @@
 		<div class="content-grid mdl-grid mdl-grid--no-spacing mdl-color--blue-100">
       <div class="mdl-cell mdl-cell--12-col mdl-cell--8-col-tablet mdl-cell--4-col-phone hero">
         <div class="mdl-grid">
-          <div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-shadow--2dp">
+          <div class="mdl-card mdl-cell mdl-cell--6-col mdl-cell--4-col-tablet mdl-shadow--6dp">
             <div class="mdl-card__title">
               <h1 class="mdl-card__title-text">Nursing Home Abuse</h1>
             </div>
             <div class="mdl-card__supporting-text">
               <p>Elder neglect, or nursing home negligence, most commonly occurs when a resident does not receive proper medical, physical, or emotional attention. As a result, neglect can pose serious risk of harm to victims. In certain cases, neglect may escalate to physical or emotional abuse.</p>
               <button class="mdl-button mdl-button--raised mdl-button--accent">I can help</button>
+              <button id="lang-switcher" class="mdl-button mdl-js-button mdl-button--icon">
+    <i class="material-icons">language</i>
+</button>
+<ul class="mdl-menu mdl-js-menu mdl-menu--top-right" for="lang-switcher">
+    <li class="mdl-menu__item">English</li>
+    <li class="mdl-menu__item">日本語</li>
+    <li class="mdl-menu__item">한국어</li>
+</ul>
             </div>
           </div>
         </div>
--- templates/footer.php
+++ templates/footer.php
@@ -3,6 +3,20 @@
   <div class="mdl-mega-footer__middle-section">
     <?php dynamic_sidebar('sidebar-mega-footer'); ?>
   </div>
+  <div vocab="http://schema.org/" typeof="Person">
+    <span property="name">Jack De La Piedra</span>
+    <span property="jobTitle">Civil Justice Attorney</span>
+    <div property="address" typeof="PostalAddress">
+      <span property="streetAddress">
+        913 Gulf Breeze Parkway, Suite 25
+      </span><br />
+      <span property="addressLocality">Gulf Breeze</span>,
+      <span property="addressRegion">FL</span>
+      <span property="postalCode">32561</span>
+    </div>
+    P: <span property="telephone">(850) 932-8560</span> F: <span property="fax">(850) 932-8129</span>
+    <a href="mailto:jack@delapiedralawfirm.com" property="email">jack@delapiedralawfirm.com</a>
+  </div>
 </footer>
 <?php } ?>
 

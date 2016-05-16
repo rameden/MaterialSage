62fd06f866c217cd9c7faf24bdbbb2c658c27b79
Mike iLL Kilmer <mike@mzoo.org>
2016-05-16 18:22:20 -0500

    Drawer menu is not layed our properly.

--- templates/footer.php
+++ templates/footer.php
@@ -25,7 +25,15 @@
       <span property="addressRegion">FL</span>
       <span property="postalCode">32561</span>
     </div>
-    P: <span property="telephone">(850) 932-8560</span> F: <span property="fax">(850) 932-8129</span><br />
+    <svg height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+        <path d="M0 0h24v24H0z" fill="none"/>
+        <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
+    </svg>
+    <span property="telephone">(850) 932-8560</span>
+    <svg height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+    <path d="M19 8H5c-1.66 0-3 1.34-3 3v6h4v4h12v-4h4v-6c0-1.66-1.34-3-3-3zm-3 11H8v-5h8v5zm3-7c-.55 0-1-.45-1-1s.45-1 1-1 1 .45 1 1-.45 1-1 1zm-1-9H6v4h12V3z"/>
+    <path d="M0 0h24v24H0z" fill="none"/>
+</svg> <span property="fax">(850) 932-8129</span><br />
     <a href="mailto:jack@delapiedralawfirm.com" property="email">jack@delapiedralawfirm.com</a>
   </div>
      <!-- <ul class="mdl-mini-footer__link-list"> -->
--- templates/header.php
+++ templates/header.php
@@ -69,12 +69,12 @@ document.addEventListener('DOMContentLoaded', function(e) { setTimeout(function(
 <header class="mdl-layout__header mdl-color--grey-800 mdl-color-text--yellow-500">
 <div class="mdl-layout__header-row mdl-layout__header-row-sliver mdl-color-text--grey-200 mdl-color--yellow-50">
   <div class="mdl-layout-spacer"></div>
-    <svg fill="#7f7d7b" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+    <svg height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
         <path d="M0 0h24v24H0z" fill="none"/>
         <path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z"/>
     </svg>
     &nbsp; <span>850 932-8560</span> &nbsp;
-    <svg fill="#7f7d7b" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
+    <svg height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
         <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z"/>
         <path d="M0 0h24v24H0z" fill="none"/>
     </svg>
